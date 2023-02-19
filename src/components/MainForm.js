fd8ca05861aaffbb5fa96ff0232d150950e47637
smschijf <210578@student.glu.nl>
2023-02-19 16:50:14 +0100

    formSection done

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,96 +1,115 @@
+import arrowRight from "../assets/img/long-arrow-right.svg";
+
 const MainForm = () => {
   return (
     <section className="mainForm">
       <form>
         <div className="formSection">
           <h2>Naam</h2>
-          <label for="voornaam" className="visuallyHidden">
-            Voornaam:
-          </label>
-          <input
-            type="text"
-            name="voornaam"
-            id="voornaam"
-            placeholder="Voornaam"
-            className="formInput"
-          />
-          <label for="achternaam" className="visuallyHidden">
-            Achternaam:
-          </label>
-          <input
-            type="text"
-            name="achternaam"
-            id="achternaam"
-            placeholder="Achternaam"
-            className="formInput"
-          />
+          <div className="inputGroup">
+            <input
+              type="text"
+              name="voornaam"
+              id="voornaam"
+              className="input"
+              autoComplete="off"
+              required
+            />
+            <label for="voornaam" className="inputLabel">
+              Voornaam
+            </label>
+          </div>
+          <div className="inputGroup">
+            <input
+              type="text"
+              name="achternaam"
+              id="achternaam"
+              className="input"
+              autoComplete="off"
+              required
+            />
+            <label for="achternaam" className="inputLabel">
+              Achternaam
+            </label>
+          </div>
         </div>
         <div className="formSection">
           <h2>Contact</h2>
-          <label for="telefoonnummer" className="visuallyHidden">
-            Telefoonnummer:
-          </label>
-          <input
-            type="tel"
-            name="telefoonnummer"
-            id="telefoonnummer"
-            placeholder="Telefoonnummer"
-            className="formInput"
-          />
-          <label for="email" className="visuallyHidden">
-            Email:
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="E-mail"
-            className="formInput"
-          />
+          <div className="inputGroup">
+            <input
+              type="tel"
+              name="telefoonnummer"
+              id="telefoonnummer"
+              className="input"
+              required
+            />
+            <label for="telefoonnummer" className="inputLabel">
+              Telefoonnummer
+            </label>
+          </div>
+          <div className="inputGroup">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="input"
+              required
+            />
+            <label for="email" className="inputLabel">
+              Email
+            </label>
+          </div>
         </div>
         <div className="formSection">
           <h2>Adres</h2>
-          <label for="adres" className="visuallyHidden">
-            Straatnaam + huisnummer:
-          </label>
-          <input
-            type="text"
-            name="adres"
-            id="adres"
-            placeholder="Straatnaam + huisnr."
-            className="formInput"
-          />
-          <label for="postcode" className="visuallyHidden">
-            Postcode:
-          </label>
-          <input
-            type="text"
-            name="postcode"
-            id="postcode"
-            placeholder="Postcode"
-            className="formInputSmall"
-          />
-          <label for="plaats" className="visuallyHidden">
-            Plaats:
-          </label>
-          <input
-            type="text"
-            name="plaats"
-            id="plaats"
-            placeholder="Plaats"
-            className="formInputMedium"
-          />
+          <div className="inputGroup">
+            <input
+              type="text"
+              name="adres"
+              id="adres"
+              className="input"
+              autoComplete="off"
+              required
+            />
+            <label for="adres" className="inputLabel">
+              Straatnaam + huisnr.
+            </label>
+          </div>
+          <div className="inputGroupSmall">
+            <input
+              type="text"
+              name="postcode"
+              id="postcode"
+              className="input"
+              autoComplete="off"
+              required
+            />
+            <label for="postcode" className="inputLabel">
+              Postcode
+            </label>
+          </div>
+          <div className="inputGroupMedium">
+            <input
+              type="text"
+              name="plaats"
+              id="plaats"
+              className="input"
+              autoComplete="off"
+              required
+            />
+            <label for="plaats" className="inputLabel">
+              Plaats
+            </label>
+          </div>
         </div>
         <div className="formSectionSubmit">
-        <label for="submit" className="visuallyHidden">
+          <label for="submit" className="visuallyHidden">
             Direct betalen
           </label>
-          <input
-            type="submit"
-            name="submit"
-            id="submit"
-            value="Direct betalen"
-          />
+          <button type="submit" name="submit" id="submit">
+            Direct betalen
+            <img src={arrowRight} alt="submit" />
+          </button>
           <a href="#">Stuur een offerte</a>
           <p>Betalen via iDEAL, Creditcard… etc.</p>
         </div>
